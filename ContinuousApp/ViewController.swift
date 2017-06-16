4dac0b39e4fbea83557550117a848a90ccd7ccd6
Hakan Yildizay [Mobil Yazilim  Servisi] <Hakan.Yildizay@ingbank.com.tr>
2017-06-16 11:45:19 +0300

    Initial Commit

---
+++ ContinuousApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContinuousApp
+//
+//  Created by Hakan Yildizay [Mobil Yazilim  Servisi] on 16/06/2017.
+//  Copyright © 2017 ING BANK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
